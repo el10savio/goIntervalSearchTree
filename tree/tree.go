e33a978ef850b95415ab31331f9a9e40f5463473
Elton SV <el10savio96@gmail.com>
2022-07-14 17:47:30 +0200

    Added Delete Functionality

--- tree/tree.go
+++ tree/tree.go
@@ -34,13 +34,17 @@ func (it *IntervalTree) Put(lo, hi int) error {
 
 // Search ...
 func (it *IntervalTree) Search(lo, hi int) *Node {
-	return search(it.root, lo, hi)
+	node, _ := search(it.root, nil, lo, hi)
+	return node
 }
 
 // Delete ...
 func (it *IntervalTree) Delete(lo, hi int) error {
 	// Search if node exists
-	node := search(it.root, lo, hi)
+	//
+	// TODO: Remove parent from signature
+	// if not required
+	node, _ := search(it.root, nil, lo, hi)
 
 	// Error out if it does not
 	if node == nil {
@@ -51,10 +55,48 @@ func (it *IntervalTree) Delete(lo, hi int) error {
 }
 
 // Delete node at point
-func delete(root *Node, value int) error {
-	// Replace with left node
-	// If left node does not exist
-	// replace with right
+func delete(lo, hi int) {
+	node, parent := search(root, nil, lo, hi)
+	if node == nil {
+		return
+	}
+
+	if node.left == nil && node.right == nil {
+		node = nil
+		return
+	}
+
+	if node.left == nil && node.right != nil {
+		node = node.right
+		return
+	}
+
+	if node.right == nil && node.left != nil {
+		node = node.left
+		return
+	}
+
+	replacement := maxNodeInLeftSubtree(node)
+	delete(replacement.Left, replacement.Right)
+
+	node.Left, node.Right = replacement.Left, replacement.Right
+}
+
+func maxNodeInLeftSubtree(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	if root.Left == nil {
+		return nil
+	}
+
+	root = root.Left
+	for root.Right != nil {
+		root = root.Right
+	}
+
+	return root
 }
 
 // put ...
@@ -80,20 +122,20 @@ func put(root *Node, lo, hi int) error {
 }
 
 // search ...
-func search(root *Node, lo, hi int) *Node {
+func search(root, parent *Node, lo, hi int) (*Node, *Node) {
 	if root == nil {
-		return nil
+		return nil, nil
 	}
 
 	if root.left == lo && root.right == hi {
-		return root
+		return root, parent
 	}
 
 	if root.left < lo {
-		return search(root.right, lo, hi)
+		return search(root.right, root, lo, hi)
 	}
 
-	return search(root.left, lo, hi)
+	return search(root.left, root, lo, hi)
 }
 
 // addNode ...
